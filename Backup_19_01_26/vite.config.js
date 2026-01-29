50873e2534bcfa8948e882e0861728a5c38613a0
Aurélien Candillier <aurelien.candillier@gmail.com>
2026-01-29 16:39:50 +0100

    test

--- Backup_19_01_26/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc' // ou '@vitejs/plugin-react' selon ton install
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Pokedex/', 
-})
